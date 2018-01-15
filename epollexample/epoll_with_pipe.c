4362cae7fb03ba40ed7edbdd7ad797ee14e5f76d
vineshtv <tv.vinesh@gmail.com>
2018-01-15 22:06:53 +0530

    Added a real loop.

--- epollexample/epoll_with_pipe.c
+++ epollexample/epoll_with_pipe.c
@@ -15,6 +15,9 @@ int main(int argc, char *argv[])
 {
     struct  epoll_event     event;
     struct  epoll_event     *events;
+    
+    /* For this example the number of messages transferred between the writer and the reader is 5 */
+    int     msgCount = 5;
 
     int     pipeFds[2]; // 0 is the read end and 1 is the write end.
     char    buf[30];
@@ -28,10 +31,14 @@ int main(int argc, char *argv[])
     pid_t   pid = fork();
 
     if(0 == pid)
-    {
+    { 
         // Child
-        sleep(5);
-        write(pipeFds[1], "test", 5);
+        int i;
+        for(i = 0; i < msgCount; i++)
+        {
+            sleep(5);
+            write(pipeFds[1], "test", 5);
+        }
         exit(0);
     }
     else if(pid > 0)
@@ -39,6 +46,7 @@ int main(int argc, char *argv[])
         // Parent
         int epollFd;
         int s;
+        int msgReceived = 0;
 
         // Create the epoll instance
         epollFd = epoll_create1(0);
@@ -46,9 +54,14 @@ int main(int argc, char *argv[])
         // Allocate events array
         events = calloc(MAXEVENTS, sizeof event);
 
-        // Register read end of pipe using edge-triggered mode with ONESHOT
+        // Register read end of pipe using edge-triggered mode.
         event.data.fd = pipeFds[0];
-        event.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
+        // EPOLLIN - The associated fd is available for read.
+        // EPOLLET - Edge triggered(ET). The default option is LEVEL_TRIGGERED(LT).
+        //              The difference between ET and LT is that in case of ET, an event will be returned only if 
+        //              there is a change in the monitored fd whereas in case of LT, it will keep returning events
+        //              until everything is read for the fd.
+        event.events = EPOLLIN | EPOLLET;
         
         s = epoll_ctl(epollFd, EPOLL_CTL_ADD, pipeFds[0], &event);
 
@@ -58,25 +71,28 @@ int main(int argc, char *argv[])
             return 1;
         }
 
+        bool done = false;
+        
         /* The event loop */
-        /* Adding a loop here even though I know that there is only one set to be read.
-         * This gives the basic idea of an event loop 
-         */
         while(1)
         {
             int n, i;
-            bool done = false;
 
             n = epoll_wait(epollFd, events, MAXEVENTS, -1);
 
             for(i = 0; i < n; i++)
             {
                 int count, s;
-
+                
                 count = read(events[i].data.fd, buf, sizeof buf);
 
                 s = write(1, buf, count);
-                done = true;    
+
+                msgReceived++;
+                if (msgReceived == msgCount)
+                {
+                    done = true;
+                }
             }
 
             if (done)
@@ -92,7 +108,7 @@ int main(int argc, char *argv[])
         // fork failed
         fprintf(stderr, "fork() failed! \n");
         return 1;
-    }
+     }
 
     return 0;
 }
