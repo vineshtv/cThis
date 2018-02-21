714d20a21375b0b2ebcaa3e6586c3dfd4bfa08f9
vineshtv <tv.vinesh@gmail.com>
2018-02-22 02:39:24 +0530

    Updated the storing of indices

--- DynamicProgramming/maxSubArraySum.c
+++ DynamicProgramming/maxSubArraySum.c
@@ -4,7 +4,7 @@
 
 #include<limits.h>
 #include<stdio.h>
-int kadane_max_subarray_sum(int a[], int n)
+void kadane_max_subarray_sum(int a[], int n)
 {
     /*
      *  The name of the algorithm is Kadane's algorithm.
@@ -27,7 +27,9 @@ int kadane_max_subarray_sum(int a[], int n)
     int global_max = INT_MIN;
     int max_till_now = 0;
     int i;
-
+    int start_index = 0; 
+    int end_index = 0;
+    int temp_start = 0;
     for(int i = 0; i < n; i++)
     {
         max_till_now += a[i];
@@ -35,25 +37,26 @@ int kadane_max_subarray_sum(int a[], int n)
         if(max_till_now > global_max)
         {
             global_max = max_till_now;
+            start_index = temp_start;
+            end_index = i;
         }
 
         if(max_till_now < 0)
         {
             max_till_now = 0;
+            temp_start = i + 1;
         }
     }
-    return global_max;
+
+    printf("Max contiguous subarray sum = %d \n", global_max);
+    printf("Start index = %d and end index = %d", start_index, end_index);
 }
 
 int main()
 {
-    int a1[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-    int a2[] = {1, 2, 3, 4, 5};
-    int a3[] = {-1, -2, -3, -4, -5};
-
-    printf("Max contiguous subarray sum for a1 = %d \n", kadane_max_subarray_sum(a1, sizeof(a1)/sizeof(a1[0])));
-    printf("Max contiguous subarray sum for a2 = %d \n", kadane_max_subarray_sum(a2, sizeof(a2)/sizeof(a2[0])));
-    printf("Max contiguous subarray sum for a3 = %d \n", kadane_max_subarray_sum(a3, sizeof(a3)/sizeof(a3[0])));
+//    int a1[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+    int a1[] = {1, 2, -3, -4, 5, -6, 7, -7};
+    kadane_max_subarray_sum(a1, sizeof(a1)/sizeof(a1[0]));
 
     return 0;
 }
